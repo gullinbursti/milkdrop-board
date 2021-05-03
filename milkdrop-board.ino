f8e27f15b4759b164d9f43513de85d177dd1306f
gullinbursti <code@solutionsmatty.com>
2021-05-03 13:09:18 -0400

    adding print screen btn logic

--- milkdrop-board.ino
+++ milkdrop-board.ino
@@ -13,13 +13,15 @@ const int PREV_PIN = 2;
 const int NEXT_PIN = 3;
 const int FADE_PIN = 4;
 const int MASH_PIN = 14;
+const int PRNT_PIN = 15;
 const int LOCK_PIN = 16;
 
 bool isFadeBtnUp = true;
 bool isMashBtnUp = true;
+bool isPrintBtnUp = true;
 bool isLockBtnUp = true;
 long curPos = 0;
-int prevRate = 1024;
+
 
 Encoder digiPot(PREV_PIN, NEXT_PIN);
 
@@ -28,10 +30,11 @@ void setup() {
   Serial.begin(9600);
   Keyboard.begin();
 
+  pinMode(RATE_PIN, INPUT_PULLUP);
   pinMode(FADE_PIN, INPUT_PULLUP);
   pinMode(MASH_PIN, INPUT);
+  pinMode(PRNT_PIN, INPUT);
   pinMode(LOCK_PIN, INPUT);
-  pinMode(RATE_PIN, INPUT_PULLUP);
 
   digiPot.write(0);
 
@@ -93,26 +96,22 @@ void loop() {
 
   if (analogRead(RATE_PIN) < 1000) {
     int rateVal = analogRead(RATE_PIN);
-    prevRate = rateVal;
 
-    Serial.print("rateVal [");
-    Serial.print(rateVal);
-    Serial.print("]");
-    Serial.println(" =-= ");
+//    Serial.print("rateVal [");
+//    Serial.print(rateVal);
+//    Serial.print("]");
+//    Serial.println(" =-= ");
 
     if (rateVal > 333) {
-      Serial.println(F("RATE--"));
+//      Serial.println(F("RATE--"));
       Keyboard.write('-');
       delay(333);
     
     } else if (rateVal > 33) {
-      Serial.println(F("RATE++"));
+//      Serial.println(F("RATE++"));
       Keyboard.write('+');
       delay(333);
     }
-  
-  } else {
-    prevRate = 1024;
   }
 
   
@@ -130,7 +129,7 @@ void loop() {
 
 
   if (digitalRead(LOCK_PIN) == HIGH && isLockBtnUp) {
-    Serial.println(F("LOCK DN"));
+//    Serial.println(F("LOCK DN"));
     isLockBtnUp = false;
     Keyboard.press(207);
     delay(33);
@@ -138,21 +137,35 @@ void loop() {
     delay(100);
   }
 
+
   if (digitalRead(LOCK_PIN) == LOW && !isLockBtnUp) {
-    Serial.println(F("LOCK UP"));
+//    Serial.println(F("LOCK UP"));
     isLockBtnUp = true;
+  }
+
+  
+  if (digitalRead(PRNT_PIN) == HIGH && isPrintBtnUp) {
+    Serial.println(F("PRINT DN"));
+    isPrintBtnUp = false;
+//    Keyboard.write('a');
+    delay(100);
+  }
+
+  if (digitalRead(PRNT_PIN) == LOW && !isPrintBtnUp) {
+    Serial.println(F("PRINT UP"));
+    isPrintBtnUp = true;
   }
 
 
   if (digitalRead(MASH_PIN) == HIGH && isMashBtnUp) {
-    Serial.println(F("MASH DN"));
+//    Serial.println(F("MASH DN"));
     isMashBtnUp = false;
     Keyboard.write('a');
     delay(100);
   }
 
   if (digitalRead(MASH_PIN) == LOW && !isMashBtnUp) {
-    Serial.println(F("MASH UP"));
+//    Serial.println(F("MASH UP"));
     isMashBtnUp = true;
   }
 }
